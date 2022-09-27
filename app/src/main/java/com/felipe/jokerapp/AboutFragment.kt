48e8b945cd36d9f96d26499475cc6ff0b7a19505
FELIPE-PESSOAL\ftiag <ftiagomelo89@gmail.com>
2022-09-27 07:56:11 -0300

    Sincronizando telas e menus com controlador de navegação

--- app/src/main/java/com/felipe/jokerapp/AboutFragment.kt
+++ app/src/main/java/com/felipe/jokerapp/AboutFragment.kt
@@ -12,6 +12,6 @@ class AboutFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
