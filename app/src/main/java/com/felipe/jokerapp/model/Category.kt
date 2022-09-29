c1cb8666d2f4e7d4b2f1a26225b58bf22872f89a
FELIPE-PESSOAL\ftiag <ftiagomelo89@gmail.com>
2022-09-29 07:09:26 -0300

    - Adicionando lib Groupie para recyclerViews; - Adicionando inicio de lista de categorias de piadas

---
+++ app/src/main/java/com/felipe/jokerapp/model/Category.kt
@@ -0,0 +1,6 @@
+package com.felipe.jokerapp.model
+
+data class Category(
+    val name: String,
+    val bgColor: Long
+)
---
+++ app/src/main/java/com/felipe/jokerapp/view/CategoryItem.kt
@@ -0,0 +1,24 @@
+package com.felipe.jokerapp.view
+
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.felipe.jokerapp.R
+import com.felipe.jokerapp.model.Category
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+
+class CategoryItem(private val category: Category) : Item<CategoryItem.CategoryViewHolder>() {
+
+    class CategoryViewHolder(view: View) : GroupieViewHolder(view)
+
+    override fun createViewHolder(itemView: View) = CategoryViewHolder(itemView)
+
+    override fun bind(viewHolder: CategoryViewHolder, position: Int) {
+        viewHolder.itemView.findViewById<TextView>(R.id.txt_category).text = category.name
+        viewHolder.itemView.findViewById<LinearLayout>(R.id.container_category)
+            .setBackgroundColor(category.bgColor.toInt())
+    }
+
+    override fun getLayout() = R.layout.item_category
+}
