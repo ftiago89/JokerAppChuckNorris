bab1f4ca09b0054c1eff4eccdffd602c45c0fa05
FELIPE-PESSOAL\ftiag <ftiagomelo89@gmail.com>
2022-09-30 22:11:07 -0300

    - Adicionando presenter para a HomeFragment; - Adicionando progressbar na HomeFragment;

--- app/src/main/java/com/felipe/jokerapp/view/HomeFragment.kt
+++ app/src/main/java/com/felipe/jokerapp/view/HomeFragment.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.felipe.jokerapp.R
 import com.felipe.jokerapp.model.Category
+import com.felipe.jokerapp.presentation.HomePresenter
 import com.xwray.groupie.GroupieAdapter
 
 class HomeFragment : Fragment() {
+
+    private lateinit var presenter: HomePresenter
+    private lateinit var progressBar: ProgressBar
+
+    private val adapter = GroupieAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = HomePresenter(this)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,14 +37,30 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        progressBar = view.findViewById(R.id.home_progress_bar)
+
         val recyclerView = view.findViewById<RecyclerView>(R.id.rv_main)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        val adapter = GroupieAdapter()
         recyclerView.adapter = adapter
 
-        adapter.add(CategoryItem(Category("testando", 0xFFFF0000)))
+        presenter.findAllCategories()
+    }
 
+    fun showCategories(categories: List<Category>) {
+        adapter.addAll(categories.map { CategoryItem(it) })
         adapter.notifyDataSetChanged()
+    }
+
+    fun showError(errString: String) {
+        Toast.makeText(requireContext(), errString, Toast.LENGTH_LONG).show()
+    }
+
+    fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun endProgress() {
+        progressBar.visibility = View.INVISIBLE
     }
 }
